8e1eb9bbd94ea84b795b7581d6e8c16b966ade1f
NP36752 <735430@student.dusd.net>
2024-11-12 09:54:33 -0800

    added animations, working on projectiles

--- 2DBeginnerAdventureP2Perales/Assets/Scripts/PlayerControllerFinal.cs
+++ 2DBeginnerAdventureP2Perales/Assets/Scripts/PlayerControllerFinal.cs
@@ -7,6 +7,9 @@ public class PlayerControllerFinal : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
     public float timeInvincible = 2;
 
     public int health { get { return currentHealth; } }
@@ -19,12 +22,16 @@ public class PlayerControllerFinal : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +39,17 @@ public class PlayerControllerFinal : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+            {
+                lookDirection.Set(move.x,  move.y);
+                lookDirection.Normalize();
+            }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
 
         if(isInvincible)
         {
@@ -41,7 +59,11 @@ public class PlayerControllerFinal : MonoBehaviour
                 isInvincible = false;
             }
         }
-    }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+    } 
     
     void FixedUpdate()
     {
@@ -64,5 +86,15 @@ public class PlayerControllerFinal : MonoBehaviour
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
