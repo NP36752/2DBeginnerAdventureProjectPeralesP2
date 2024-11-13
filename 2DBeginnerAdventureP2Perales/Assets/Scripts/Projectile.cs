8c9b25b50b0fa50974c2838020c65cd373bfdfa7
NP36752 <735430@student.dusd.net>
2024-11-12 22:19:44 -0800

    q

--- 2DBeginnerAdventureP2Perales/Assets/Scripts/Projectile.cs
+++ 2DBeginnerAdventureP2Perales/Assets/Scripts/Projectile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
